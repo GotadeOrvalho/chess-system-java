e3998d0b4b3f33d0cf705e7606cd0ee699cd1887
GotadeOrvalho <adrianeferreirap@gmail.com>
2024-07-25 06:53:24 -0300

    Starting to impement Board and Piece

---
+++ src/boardgame/Piece.java
@@ -0,0 +1,24 @@
+package boardgame;
+
+public class Piece {
+	protected Position position;
+	private Board board;
+
+	
+
+	protected Board getBoard() {
+		return board;
+	}
+
+	
+
+	public Piece() {
+
+	}
+
+	public Piece( Board board) {
+
+		this.board = board;
+	}
+
+}
