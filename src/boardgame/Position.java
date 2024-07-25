daa2bc69e19c03fec4e4391065a4ba84217b1cf8
GotadeOrvalho <adrianeferreirap@gmail.com>
2024-07-25 06:39:19 -0300

    First class: Position

---
+++ src/boardgame/Position.java
@@ -0,0 +1,38 @@
+package boardgame;
+
+public class Position {
+	private int row;
+	private int columm;
+
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public int getColumm() {
+		return columm;
+	}
+
+	public void setColumm(int columm) {
+		this.columm = columm;
+	}
+
+	public Position() {
+
+	}
+
+	public Position(int row, int columm) {
+
+		this.row = row;
+		this.columm = columm;
+	}
+
+	@Override
+	public String toString() {
+		return row + ", " + columm;
+	}
+
+}
